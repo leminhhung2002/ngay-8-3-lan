c500315ada42221499efb0e243e9f3d1d1962d52
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2022-03-07 20:21:25 +0700

    Add files via upload
    lan

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'LAN';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng người tớ thương';
+const contentCard = 'Chúc Lan 8-3 vui vẻ luôn luôn gặp may mắn trong cuộc sống ,thành công  ,không có một chút chuyện buồn nào nữa ,luôn xinh gái ,đáng yêu nha <3';
+
+const giftImage = '1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
